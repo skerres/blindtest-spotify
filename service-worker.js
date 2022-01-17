7db92b6350dde74a87ed1950443072344eba1436
Kerres, Sebastian (TM-R) <sebastian.kerres@iav.de>
2022-01-17 14:33:42 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blindtest-spotify/precache-manifest.af1d10f296fb8bac425bec0e56195ae1.js"
+  "/blindtest-spotify/precache-manifest.261fd5e6468f2402c7bd70252791f0b9.js"
 );
 
 self.addEventListener('message', (event) => {
